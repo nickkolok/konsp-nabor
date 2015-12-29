8b929594f628795f57a20a88872c38cc88859f96
nickkolok <nickkolok@mail.ru>
2015-12-29 18:19:44 +0300

    Возможность добавления клавиш

--- main.js
+++ main.js
@@ -9,19 +9,22 @@ var defaultKeyboardSet=[
 ];
 //TODO: сделать хоть сколь-нибудь осмысленным
 
+function createKeyboardKey(props){
+	var targetKey=$(
+			'<span class="keyboard-key" data-texcode="'+
+				props[1]+'" data-htmlcode="'+
+				props[0]+'">'+props[0]+'</span>'
+		);
+	targetKey[0].onclick=generateClickHandler(props[1]);
+	return targetKey;
+}
 
 function createKeyboardSet(keyset){
 	var targetElement=$('#keyboard-container');
 	for(var row=0; row<keyset.length; row++){
 		var targetRow=$('<div class="keyboard-row"></div>');
 		for(var key=0; key<keyset[row].length; key++){
-			var targetKey=$(
-				'<span class="keyboard-key" data-texcode="'+
-					keyset[row][key][1]+'" data-htmlcode="'+
-					keyset[row][key][0]+'">'+keyset[row][key][0]+'</span>'
-			);
-			targetKey[0].onclick=generateClickHandler(keyset[row][key][1]);
-			targetRow.append(targetKey);
+			targetRow.append(createKeyboardKey(keyset[row][key]));
 		}
 		targetElement.append(targetRow);
 	}
@@ -81,7 +84,6 @@ function loadKeyboardSetFromLocalStorage(){
 	var storedKeyboardSet;
 	try{
 		storedKeyboardSet=JSON.parse(localStorage['texkeyboard-keyset']);
-		console.log(storedKeyboardSet);
 		if(!storedKeyboardSet || storedKeyboardSet.length==0)
 			storedKeyboardSet = defaultKeyboardSet;
 	}catch(e){
@@ -93,6 +95,15 @@ function loadKeyboardSetFromLocalStorage(){
 function addKeyboardRow(){
 	$('#keyboard-container').append($('<div class="keyboard-row"></div>'));
 	makeKeyboardSortable();
+}
+
+function addKeyboardKey(){
+	var rows = $('#keyboard-container .keyboard-row');
+	var lastRow = $(rows[rows.length-1]); //TODO: last на jQuery
+	var newKey = createKeyboardKey([ $('#adding-html').val(), $('#adding-tex').val() ]);
+	lastRow.append(newKey);
+	makeKeyboardSortable();
+	MathJax.Hub.Typeset(newKey[0]);
 }
 
 function onUnload(){
